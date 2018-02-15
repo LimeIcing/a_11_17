208517ac6397e2be1712131a5ba8a8d742b24a84
Lime Icing <emil577b@stud.kea.dk>
2018-02-15 19:16:33 +0100

    Assignment prepped

---
+++ Main17.java
@@ -0,0 +1,19 @@
+import java.util.Map;
+
+public class Main17 {
+
+    /*
+    Write a method called subMap that accepts two maps from strings to strings as its parameters and returns true if
+    every key in the first map is also contained in the second map and maps to the same value in the second map. For
+    example, {Smith=949–0504, Marty=206–9024} is a submap of
+    {Marty=206–9024, Hawking=123–4567, Smith=949–0504, Newton=123–4567} . The empty map is a submap of every map.
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean subMap(Map<String, String> m, Map<String, String> n) {
+
+    }
+}
